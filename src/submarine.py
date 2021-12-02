d43090b0fff02da3b723f516584006ad1d5586e9
Paul Kerchen <paul@whirlingchair.com>
2021-12-02 12:48:57 -0500

    Implemented first part of day 2 puzzle

--- src/submarine.py
+++ src/submarine.py
@@ -14,3 +14,23 @@ class Submarine:
             elif command == 'up':
                 self.depth -= int(amount)
 
+
+def read_command_data(command_file: str) -> list:
+    command_data = []
+
+    with open(command_file, "r") as cfile:
+        for line in cfile:
+            command_data.append(line.strip())
+    return command_data
+
+
+def command_sub(command_file: str):
+    command_data = read_command_data(command_file)
+    boat = Submarine()
+
+    boat.move(command_data)
+
+    print(f"Final position: {boat.position}")
+    print(f"Final depth: {boat.depth}")
+    print(f"Product: {boat.depth * boat.position}")
+
