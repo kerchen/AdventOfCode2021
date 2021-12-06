69be802dd9517b30502384fcedf1a18886065af7
Paul Kerchen <paul@whirlingchair.com>
2021-12-06 11:20:23 -0500

    Implemented spawning

--- src/lanternfish.py
+++ src/lanternfish.py
@@ -4,7 +4,32 @@ class Lanternfish:
     def __init__(self, initial_timer_value: int):
         self.timer = initial_timer_value
 
-    def advance_age(self):
+    def advance_age(self) -> bool:
+        spawned = False
         self.timer -= 1
         if self.timer < 0:
             self.timer = 6
+            spawned = True
+        return spawned
+
+
+def simulate_fish(initial_state: list, days: int):
+    population = []
+    for initial_timer_value in initial_state:
+        population.append(Lanternfish(initial_timer_value))
+
+    for day in range(days):
+        spawn_count = 0
+
+        def age_fish(fish: Lanternfish):
+            nonlocal spawn_count
+            if fish.advance_age():
+                spawn_count += 1
+
+        [age_fish(fish) for fish in population]
+
+        while spawn_count > 0:
+            population.append(Lanternfish(8))
+            spawn_count -= 1
+
+    return [fish.timer for fish in population]
--- test/lanternfish_test.py
+++ test/lanternfish_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lanternfish import Lanternfish
+from lanternfish import Lanternfish, simulate_fish
 
 
 fish_timer_test_data = [
@@ -18,3 +18,31 @@ def test_fish_timer_progression(initial_fish_timer_value, expected_next_timer_va
     fish.advance_age()
     assert fish.timer == expected_next_timer_value
 
+
+fish_spawn_test_data = [
+    (3, False),
+    (1, False),
+    (0, True),
+    (8, False)
+]
+
+
+@pytest.mark.parametrize("initial_fish_timer_value, expected_spawn", fish_spawn_test_data)
+def test_fish_spawning(initial_fish_timer_value, expected_spawn):
+    fish = Lanternfish(initial_fish_timer_value)
+
+    spawn = fish.advance_age()
+    assert spawn == expected_spawn
+
+
+population_progression_test_data = [
+    ([3, 1, 5], 1, [2, 0, 4]),
+    ([6, 4, 5], 3, [3, 1, 2]),
+    ([6, 4, 0], 1, [5, 3, 6, 8])
+]
+
+
+@pytest.mark.parametrize("initial_fish_timer_values, days_to_simulate, expected_timer_values", population_progression_test_data)
+def test_fish_population_progression(initial_fish_timer_values, days_to_simulate, expected_timer_values):
+    computed_timer_values = simulate_fish(initial_fish_timer_values, days_to_simulate)
+    assert computed_timer_values == expected_timer_values
