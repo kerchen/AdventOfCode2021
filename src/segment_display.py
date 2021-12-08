a4f9f667f611d6bb0b475e17137be8a259cdafdd
Paul Kerchen <paul@whirlingchair.com>
2021-12-08 10:57:34 -0500

    First problem solved

--- src/segment_display.py
+++ src/segment_display.py
@@ -3,3 +3,21 @@
 def parse_input_line(input_line: str) -> tuple:
     signal_pattern_str, display_value_str = input_line.split(' | ')
     return signal_pattern_str.split(' '), display_value_str.split(' ')
+
+
+def solve_p1(input_data_file: str):
+    with open(input_data_file, "r") as dfile:
+        signal_patterns = []
+        display_values = []
+        for line in dfile:
+            pattern, value = parse_input_line(line.strip())
+            signal_patterns.append(pattern)
+            display_values.append(value)
+
+        unique_pattern_lengths = [2, 3, 4, 7]
+        count = 0
+        for value_list in display_values:
+            for v in value_list:
+                if len(v) in unique_pattern_lengths:
+                    count += 1
+        print(f"Number of display values with unique pattern lengths: {count}")
