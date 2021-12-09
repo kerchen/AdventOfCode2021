f8df756b5d4e53232b7131008edcff88b03cae3a
Paul Kerchen <paul@whirlingchair.com>
2021-12-09 12:17:44 -0500

    Day 9 is in the rear view mirror.

--- src/heightmap.py
+++ src/heightmap.py
@@ -10,7 +10,7 @@ class Heightmap:
                 self.heightmap[(c, r)] = int(h)
         print(f"Row count: {self.row_count}\nCol count: {self.col_count}")
 
-    def is_lower_than_neighbors(self, c, r):
+    def is_lower_than_neighbors(self, c, r) -> bool:
         max_height = 10
         height = self.heightmap.get((c, r))
         if self.heightmap.get((c, r - 1), max_height) <= height:
@@ -24,18 +24,52 @@ class Heightmap:
 
         return True
 
-    def find_low_points(self):
+    def find_low_points(self) -> list:
         low_points = []
+        low_point_coords = []
         for r in range(self.row_count):
             for c in range(self.col_count):
                 if self.is_lower_than_neighbors(c, r):
                     low_points.append(self.heightmap.get((c, r)))
+                    low_point_coords.append((c, r))
 
-        return low_points
+        return low_points, low_point_coords
 
-    def compute_risk_level(self):
-        low_points = self.find_low_points()
+    def compute_risk_level(self) -> int:
+        low_points, _ = self.find_low_points()
         return sum(low_points) + len(low_points)
+
+    def add_to_basin(self, members, possible_member):
+        if possible_member in members:
+            return members
+
+        edge_height = 9
+        if self.heightmap.get(possible_member, edge_height) < edge_height:
+            members.add(possible_member)
+            members = self.grow_basin(members, possible_member)
+        return members
+
+    def grow_basin(self, members, from_point):
+        members.add(from_point)
+        members = self.add_to_basin(members, (from_point[0] + 1, from_point[1]))
+        members = self.add_to_basin(members, (from_point[0] - 1, from_point[1]))
+        members = self.add_to_basin(members, (from_point[0], from_point[1] + 1))
+        members = self.add_to_basin(members, (from_point[0], from_point[1] - 1))
+        return members
+
+    def find_basin_size(self, start_point) -> int:
+        basin_members = set()
+        basin_members = self.grow_basin(basin_members, start_point)
+
+        return len(basin_members)
+
+    def find_basin_sizes(self) -> list:
+        low_points, coords = self.find_low_points()
+        basin_sizes = []
+        for start_point in coords:
+            basin_sizes.append(self.find_basin_size(start_point))
+
+        return basin_sizes
 
 
 def solve(input_data_file: str):
@@ -45,4 +79,8 @@ def solve(input_data_file: str):
             height_data.append(line.strip())
 
         hm = Heightmap(height_data)
+        basin_sizes = sorted(hm.find_basin_sizes(), reverse=True)
+
         print(f"Risk level: {hm.compute_risk_level()}")
+        print(f"Three largest basin sizes: {basin_sizes[0:3]}")
+        print(f"Product: {basin_sizes[0] * basin_sizes[1] * basin_sizes[2]}")
--- test/heightmap_test.py
+++ test/heightmap_test.py
@@ -30,5 +30,96 @@ low_point_test_data = [
 def test_low_point_calculation(input_data, expected_low_points):
     hm = Heightmap(input_data)
 
-    low_points = hm.find_low_points()
+    low_points, _ = hm.find_low_points()
     assert low_points == expected_low_points
+
+
+low_point_coordinate_test_data = [
+    (["2199943210",
+      "3987894921",
+      "9856789892",
+      "8767896789",
+      "9899965678"], [(1, 0), (9, 0), (2, 2), (6, 4)]),
+    (["30987894921",
+      "98567809892",
+      "80678960789"], [(1, 0), (7, 0), (10, 0), (2, 1), (6, 1), (1, 2), (7, 2)]),
+    (["309",
+      "985",
+      "806"], [(1, 0), (2, 1), (1, 2)]),
+    (["3096",
+      "9850",
+      "8068",
+      "4319"], [(1, 0), (3, 1), (1, 2), (2, 3)]),
+    (["9999",
+      "8888",
+      "7777",
+      "6776"], [(0, 3), (3, 3)]),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_low_point_coords", low_point_coordinate_test_data)
+def test_low_point_coordinates(input_data, expected_low_point_coords):
+    hm = Heightmap(input_data)
+
+    _, coords = hm.find_low_points()
+    assert coords == expected_low_point_coords
+
+
+basin_count_test_data = [
+    (["999",
+      "909",
+      "999"], 1),
+    (["30999999921",
+      "99967909992",
+      "80699990789"], 6),
+    (["2199943210",
+      "3987894921",
+      "9856789892",
+      "8767896789",
+      "9899965678"], 4),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_basin_count", basin_count_test_data)
+def test_basin_count(input_data, expected_basin_count):
+    hm = Heightmap(input_data)
+
+    basins = hm.find_basin_sizes()
+    assert len(basins) == expected_basin_count
+
+
+basin_size_test_data = [
+    (["999",
+      "909",
+      "999"], [1]),
+    (["99999",
+      "90909",
+      "99999"], [1, 1]),
+    (["999999",
+      "909019",
+      "999999"], [1, 2]),
+    (["9999999",
+      "9096019",
+      "9999999"], [1, 3]),
+    (["9999999",
+      "9596519",
+      "9096019",
+      "9496219",
+      "9999999"], [3, 9]),
+    (["30999999921",
+      "99967909992",
+      "80699990789"], [2, 3, 2, 1, 3, 3]),
+    (["2199943210",
+      "3987894921",
+      "9856789892",
+      "8767896789",
+      "9899965678"], [3, 9, 14, 9]),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_basin_sizes", basin_size_test_data)
+def test_basin_sizes(input_data, expected_basin_sizes):
+    hm = Heightmap(input_data)
+
+    basins = hm.find_basin_sizes()
+    assert basins == expected_basin_sizes
