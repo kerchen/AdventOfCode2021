8231a3ab9747a2aeab2e506b9432b87597131e54
Paul Kerchen <paul@whirlingchair.com>
2021-12-08 19:28:51 -0500

    Day 8 done and dusted.

--- src/segment_display.py
+++ src/segment_display.py
@@ -5,19 +5,87 @@ def parse_input_line(input_line: str) -> tuple:
     return signal_pattern_str.split(' '), display_value_str.split(' ')
 
 
-def solve_p1(input_data_file: str):
+def get_set_for_length(signal_patterns: list, length: int) -> set:
+    for pattern in signal_patterns:
+        if len(pattern) == length:
+            return set(pattern)
+
+    return None
+
+
+def deduce_a(signal_patterns: list) -> set:
+    cf_set = get_set_for_length(signal_patterns, 2)
+    acf_set = get_set_for_length(signal_patterns, 3)
+
+    return acf_set - cf_set
+
+
+def deduce_bd(signal_patterns: list) -> set:
+    cf_set = get_set_for_length(signal_patterns, 2)
+    bcdf_set = get_set_for_length(signal_patterns, 4)
+
+    return bcdf_set - cf_set
+
+
+def deduce_eg(signal_patterns: list) -> set:
+    acf_set = get_set_for_length(signal_patterns, 3)
+    bcdf_set = get_set_for_length(signal_patterns, 4)
+    all_set = get_set_for_length(signal_patterns, 7)
+
+    return all_set - bcdf_set - acf_set
+
+
+def deduce_digits(signal_patterns: list, display_values: list) -> int:
+    eg_set = deduce_eg(signal_patterns)
+    bd_set = deduce_bd(signal_patterns)
+    acf_set = get_set_for_length(signal_patterns, 3)
+
+    return_digits = 0
+    for dv in display_values:
+        if len(dv) == 2:
+            return_digits = return_digits * 10 + 1
+        elif len(dv) == 3:
+            return_digits = return_digits * 10 + 7
+        elif len(dv) == 4:
+            return_digits = return_digits * 10 + 4
+        elif len(dv) == 7:
+            return_digits = return_digits * 10 + 8
+        elif len(dv) == 5:
+            if eg_set.intersection(set(dv)) == eg_set:
+                return_digits = return_digits * 10 + 2
+            elif bd_set.intersection(set(dv)) == bd_set:
+                return_digits = return_digits * 10 + 5
+            else:
+                return_digits = return_digits * 10 + 3
+        else:
+            if not bd_set.intersection(set(dv)) == bd_set:
+                return_digits = return_digits * 10 + 0
+            elif acf_set.intersection(set(dv)) == acf_set:
+                return_digits = return_digits * 10 + 9
+            else:
+                return_digits = return_digits * 10 + 6
+
+    return return_digits
+
+
+def solve(input_data_file: str):
     with open(input_data_file, "r") as dfile:
         signal_patterns = []
         display_values = []
+        digits_sum = 0
         for line in dfile:
             pattern, value = parse_input_line(line.strip())
             signal_patterns.append(pattern)
             display_values.append(value)
+            digits_sum += deduce_digits(pattern, value)
 
-        unique_pattern_lengths = [2, 3, 4, 7]
+        unique_patterns = {2: 'cf', 3: 'acf', 4: 'bcdf', 7: 'abcdefg'}
         count = 0
         for value_list in display_values:
             for v in value_list:
-                if len(v) in unique_pattern_lengths:
+                pattern_length = len(v)
+                if pattern_length in unique_patterns:
                     count += 1
+
         print(f"Number of display values with unique pattern lengths: {count}")
+        print(f"Sum of digits: {digits_sum}")
--- test/segment_display_test.py
+++ test/segment_display_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from segment_display import parse_input_line
+from segment_display import parse_input_line, deduce_a, deduce_bd, deduce_eg, deduce_digits
 
 
 parse_signal_pattern_test_data = [
@@ -15,4 +15,87 @@ def test_input_parser(input_data, expected_signal_pattern, expected_display_valu
     signal_pattern, display_values = parse_input_line(input_data)
     assert signal_pattern == expected_signal_pattern
     assert display_values == expected_display_values
+
+
+a_deduction_test_data = [
+    ("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", set(['d'])),
+    ("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", set(['b'])),
+    ("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", set(['b'])),
+    ("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", set(['d'])),
+    ("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", set(['b'])),
+    ("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", set(['f'])),
+    ("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", set(['d'])),
+    ("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", set(['c'])),
+    ("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", set(['b'])),
+    ("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", set(['c'])),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_deduced_a", a_deduction_test_data)
+def test_a_deduction(input_data, expected_deduced_a):
+    signal_pattern, _ = parse_input_line(input_data)
+    deduced_a = deduce_a(signal_pattern)
+    assert deduced_a == expected_deduced_a
+
+bd_deduction_test_data = [
+    ("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", set(['c', 'g'])),
+    ("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", set(['e', 'f'])),
+    ("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",  set(['a', 'f'])),
+    ("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", set(['a', 'f'])),
+    ("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", set(['c', 'e'])),
+    ("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", set(['b', 'e'])),
+    ("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", set(['c', 'e'])),
+    ("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", set(['b', 'f'])),
+    ("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", set(['d', 'e'])),
+    ("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", set(['a', 'e'])),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_deduced_bd", bd_deduction_test_data)
+def test_bd_deduction(input_data, expected_deduced_bd):
+    signal_pattern, _ = parse_input_line(input_data)
+    deduced_bd = deduce_bd(signal_pattern)
+    assert deduced_bd == expected_deduced_bd
+
+
+eg_deduction_test_data = [
+    ("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", set(['a', 'f'])),
+    ("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", set(['a', 'd'])),
+    ("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",  set(['d', 'e'])),
+    ("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", set(['e', 'g'])),
+    ("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", set(['a', 'd'])),
+    ("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", set(['d', 'g'])),
+    ("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", set(['a', 'b'])),
+    ("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", set(['a', 'g'])),
+    ("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", set(['a', 'f'])),
+    ("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", set(['b', 'd'])),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_deduced_eg", eg_deduction_test_data)
+def test_eg_deduction(input_data, expected_deduced_eg):
+    signal_pattern, _ = parse_input_line(input_data)
+    deduced_eg = deduce_eg(signal_pattern)
+    assert deduced_eg == expected_deduced_eg
+
+
+final_deduction_test_data = [
+    ("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 8394),
+    ("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 9781),
+    ("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", 1197),
+    ("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", 9361),
+    ("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", 4873),
+    ("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", 8418),
+    ("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", 4548),
+    ("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", 1625),
+    ("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", 8717),
+    ("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", 4315),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_deduced_digits", final_deduction_test_data)
+def test_final_deduction(input_data, expected_deduced_digits):
+    signal_pattern, display_digits = parse_input_line(input_data)
+    deduced_digits = deduce_digits(signal_pattern, display_digits)
+    assert deduced_digits == expected_deduced_digits
 
