ec0381146787ca7288ebec1a97ac0f8daf87d96d
Paul Kerchen <paul@whirlingchair.com>
2021-12-12 10:11:52 -0500

    Two-path case working

--- src/tsp.py
+++ src/tsp.py
@@ -7,12 +7,37 @@ class CaveSystem:
             node_names = link.split('-')
             for i in range(2):
                 if node_names[i] not in self.nodes.keys():
-                    self.nodes[node_names[i]] = []
-            self.nodes[node_names[0]].append(node_names[1])
-            self.nodes[node_names[1]].append(node_names[0])
+                    self.nodes[node_names[i]] = set()
+            self.nodes[node_names[0]].add(node_names[1])
+            self.nodes[node_names[1]].add(node_names[0])
 
     def node_count(self) -> int:
         return len(self.nodes.keys())
 
     def are_directly_connected(self, a_node: str, b_node: str) -> bool:
         return b_node in self.nodes[a_node]
+
+    def find_all_paths(self):
+        all_paths = []
+        current_path = []
+
+        def can_extend(cave, path):
+            if cave not in path:
+                return True
+            return False
+
+        def follow_path(cave: str, path: list, found_paths) -> bool:
+            if not can_extend(cave, path):
+                return
+
+            path.append(cave)
+            if cave == 'end':
+                found_paths.append(path)
+                return
+
+            for adjacent_cave in self.nodes[cave]:
+                path_trial = path.copy()
+                follow_path(adjacent_cave, path_trial, found_paths)
+
+        follow_path('start', current_path, all_paths)
+        return all_paths
--- test/tsp_test.py
+++ test/tsp_test.py
@@ -7,6 +7,12 @@ path_parse_test_data = [
    (["start-A",
      "A-end"],
     3),
+    (["start-A",
+      "A-b",
+      "b-c",
+      "c-D",
+      "A-end"],
+     6),
 ]
 
 
@@ -24,6 +30,9 @@ path_direct_connectivity_test_data = [
      'start', 'A', True),
     (["start-A",
       "A-end"],
+     'end', 'A', True),
+    (["start-A",
+      "A-end"],
      'start', 'end', False),
     (["start-A",
       "A-b",
@@ -38,3 +47,27 @@ def test_direct_connectivity_of_input_nodes(input_data, first_node, second_node,
 
     directly_connected = cave_system.are_directly_connected(first_node, second_node)
     assert directly_connected == expected_directly_connected
+
+
+all_paths_test_data = [
+    (["start-A",
+      "A-end"],
+     ["start,A,end"]),
+    (["start-A",
+      "A-b",
+      "b-end"],
+     ["start,A,b,end"]),
+    (["start-A",
+      "A-end",
+      "start-B",
+      "B-end"],
+     ["start,A,end", "start,B,end"]),
+]
+
+
+@pytest.mark.parametrize("input_data, expected_paths", all_paths_test_data)
+def test_all_paths_found(input_data, expected_paths):
+    cave_system = CaveSystem(input_data)
+
+    all_paths = cave_system.find_all_paths()
+    assert sorted([','.join(p) for p in all_paths]) == sorted(expected_paths)
