907709245cc00bebeba15caa1aea040dd536da9b
Paul Kerchen <paul@whirlingchair.com>
2021-12-20 17:46:43 -0500

    First attempt at first puzzle--wrong!

--- src/image_enhance.py
+++ src/image_enhance.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Point:
     x: int
     y: int
@@ -24,6 +24,23 @@ class Image:
                 if pixel_value == '#':
                     self.lit_pixels[Point(c, r)] = True
 
+    def __eq__(self, other):
+        if not self.lit_pixels and not other.lit_pixels:
+            return True
+        if not len(self.lit_pixels) == len(other.lit_pixels):
+            return False
+        my_pixels = sorted(self.lit_pixels)
+        other_pixels = sorted(other.lit_pixels)
+        dx = my_pixels[0].x - other_pixels[0].x
+        dy = my_pixels[0].y - other_pixels[0].y
+        for i in range(len(my_pixels)):
+            if not (my_pixels[i].x - dx) == other_pixels[i].x:
+                return False
+            if not (my_pixels[i].y - dy) == other_pixels[i].y:
+                return False
+
+        return True
+
     def algorithm_index(self, coordinate: Point) -> int:
         index = 0
         for y in range(coordinate.y-1, coordinate.y+2):
@@ -33,3 +50,26 @@ class Image:
                     index += 1
 
         return index
+
+    def apply_algorithm(self, algorithm: str):
+        new_image = Image([])
+
+        for k in sorted(self.lit_pixels):
+            for y in range(k.y-1, k.y+2):
+                for x in range(k.x-1, k.x+2):
+                    index = self.algorithm_index(Point(x, y))
+                    if algorithm[index] == '#':
+                        new_image.lit_pixels[Point(x, y)] = True
+
+        return new_image
+
+
+def solve(input_data_file: str):
+    with open(input_data_file, "r") as dfile:
+        raw_input = dfile.read()
+        algorithm, image_data = parse_input(raw_input)
+        image = Image(image_data)
+        first_gen_image = image.apply_algorithm(algorithm)
+        second_gen_image = first_gen_image.apply_algorithm(algorithm)
+
+        print(f"Number of pixels on in second generation: {len(second_gen_image.lit_pixels)}")
--- test/image_enhance_test.py
+++ test/image_enhance_test.py
@@ -47,3 +47,104 @@ def test_coordinate_to_index_computation_is_correct(input_image, coordinate, exp
     index = image.algorithm_index(coordinate)
 
     assert index == expected_index
+
+
+image_equality_test_data = [
+    ([], [], True),
+    ([], ['#'], False),
+    (['#'], ['#'], True),
+    (['.##'], ['#'], False),
+    (['.##',
+      '##.'],
+     ['##.',
+      '.##'],
+     False),
+    (['.#.',
+      '##.',
+      '#.#'],
+     ['......',
+      '..#...',
+      '.##...',
+      '.#.#..',
+      '.....'],
+     True)
+]
+
+
+@pytest.mark.parametrize('input_image1, input_image2, expected_equal', image_equality_test_data)
+def test_image_equality_is_correct(input_image1, input_image2, expected_equal):
+    image1 = Image(input_image1)
+    image2 = Image(input_image2)
+
+    if expected_equal:
+        assert image1 == image2
+    else:
+        assert not image1 == image2
+
+example_algorithm_str = '..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#'
+
+apply_algorithm_test_data = [
+    (example_algorithm_str,
+     ['.#.'],
+     ['.##',
+      '#.#',
+      '.#.']),
+    (example_algorithm_str,
+     ['...............',
+      '.....#..#......',
+      '.....#.........',
+      '.....##..#.....',
+      '.......#.......',
+      '.......###.....'],
+     ['...............',
+      '...............',
+      '...............',
+      '...............',
+      '.....##.##.....',
+      '....#..#.#.....',
+      '....##.#..#....',
+      '....####..#....',
+      '.....#..##.....',
+      '......##..#....',
+      '.......#.#.....',
+      '...............',
+      '...............',
+      '...............',
+      '...............']
+     ),
+    (example_algorithm_str,
+     ['...............',
+      '...............',
+      '...............',
+      '...............',
+      '.....##.##.....',
+      '....#..#.#.....',
+      '....##.#..#....',
+      '....####..#....',
+      '.....#..##.....',
+      '......##..#....',
+      '.......#.#.....',
+      '...............',
+      '...............',
+      '...............',
+      '...............'],
+     ['...............',
+      '..........#....',
+      '....#..#.#.....',
+      '...#.#...###...',
+      '...#...##.#....',
+      '...#.....#.#...',
+      '....#.#####....',
+      '.....#.#####...',
+      '......##.##....',
+      '.......###.....',
+      '...............'])
+]
+
+
+@pytest.mark.parametrize('input_algorithm, input_image_data, expected_image_data', apply_algorithm_test_data)
+def test_applying_enhancement_algorithm_produces_correct_image(input_algorithm, input_image_data, expected_image_data):
+    image = Image(input_image_data)
+    expected_image = Image(expected_image_data)
+    enhanced_image = image.apply_algorithm(input_algorithm)
+    assert enhanced_image == expected_image
