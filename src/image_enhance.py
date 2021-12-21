f8354781294f0e4160622b684f757b79777677b3
Paul Kerchen <paul@whirlingchair.com>
2021-12-21 15:27:41 -0500

    The good news: First puzzle of day 20 solved! The bad news: this code is super fugly!

--- src/image_enhance.py
+++ src/image_enhance.py
@@ -1,10 +1,33 @@
 from dataclasses import dataclass
+from collections import Counter
 
 
 @dataclass(frozen=True, order=True)
 class Point:
     x: int
     y: int
+
+
+@dataclass
+class ThawedPoint:
+    x: int
+    y: int
+
+
+@dataclass
+class Rect:
+    upper_left: ThawedPoint
+    lower_right: ThawedPoint
+
+    def grow(self, pt: Point):
+        if self.upper_left.x > pt.x:
+            self.upper_left.x = pt.x
+        if self.upper_left.y > pt.y:
+            self.upper_left.y = pt.y
+        if self.lower_right.x < pt.x:
+            self.lower_right.x = pt.x
+        if self.lower_right.y < pt.y:
+            self.lower_right.y = pt.y
 
 
 def parse_input(input_data: str) -> tuple:
@@ -16,54 +39,83 @@ def parse_input(input_data: str) -> tuple:
     return algorithm, image
 
 
+def find_next(start, lit_pixels, keys):
+    i = start
+    while i < len(keys):
+        if lit_pixels[keys[i]]:
+            break
+        i += 1
+
+    return i
+
+
 class Image:
     def __init__(self, raw_data: list[str]):
         self.lit_pixels = {}
+        self.bounds = Rect(ThawedPoint(100000, 100000), ThawedPoint(-10000, -10000))
+        self.infinite_pixels_on = False
+
         for r, row in enumerate(raw_data):
             for c, pixel_value in enumerate(row):
-                if pixel_value == '#':
-                    self.lit_pixels[Point(c, r)] = True
+                pt = Point(c, r)
+                self.bounds.grow(pt)
+                self.lit_pixels[pt] = (pixel_value == '#')
 
     def __eq__(self, other):
         if not self.lit_pixels and not other.lit_pixels:
             return True
-        if not len(self.lit_pixels) == len(other.lit_pixels):
+        if not Counter(self.lit_pixels.values())[True] == Counter(other.lit_pixels.values())[True]:
             return False
-        my_pixels = sorted(self.lit_pixels)
-        other_pixels = sorted(other.lit_pixels)
-        dx = my_pixels[0].x - other_pixels[0].x
-        dy = my_pixels[0].y - other_pixels[0].y
-        for i in range(len(my_pixels)):
-            if not (my_pixels[i].x - dx) == other_pixels[i].x:
+        my_keys = sorted(self.lit_pixels)
+        my_i = find_next(0, self.lit_pixels, my_keys)
+        other_keys = sorted(other.lit_pixels)
+        other_i = find_next(0, other.lit_pixels, other_keys)
+        dx = my_keys[my_i].x - other_keys[other_i].x
+        dy = my_keys[my_i].y - other_keys[other_i].y
+        my_i = find_next(my_i+1, self.lit_pixels, my_keys)
+        other_i = find_next(other_i+1, other.lit_pixels, other_keys)
+        while my_i < len(my_keys):
+            if not (my_keys[my_i].x - dx) == other_keys[other_i].x:
                 return False
-            if not (my_pixels[i].y - dy) == other_pixels[i].y:
+            if not (my_keys[my_i].y - dy) == other_keys[other_i].y:
                 return False
+            my_i = find_next(my_i+1, self.lit_pixels, my_keys)
+            other_i = find_next(other_i+1, other.lit_pixels, other_keys)
 
         return True
+
+    def dump(self):
+        for y in range(self.bounds.upper_left.y, self.bounds.lower_right.y + 1):
+            for x in range(self.bounds.upper_left.x, self.bounds.lower_right.x + 1):
+                pt = Point(x, y)
+                print('#' if self.lit_pixels.get(pt, self.infinite_pixels_on) else '.', end='')
+            print('')
 
     def algorithm_index(self, coordinate: Point) -> int:
         index = 0
         for y in range(coordinate.y-1, coordinate.y+2):
             for x in range(coordinate.x-1, coordinate.x+2):
                 index *= 2
-                if self.lit_pixels.get(Point(x, y), False):
+                if self.lit_pixels.get(Point(x, y), self.infinite_pixels_on):
                     index += 1
 
         return index
 
     def apply_algorithm(self, algorithm: str):
         new_image = Image([])
-        already_computed = set()
 
-        for k in sorted(self.lit_pixels):
-            for y in range(k.y-1, k.y+2):
-                for x in range(k.x-1, k.x+2):
-                    pt = Point(x, y)
-                    if pt not in already_computed:
-                        already_computed.add(pt)
-                        index = self.algorithm_index(pt)
-                        if algorithm[index] == '#':
-                            new_image.lit_pixels[pt] = True
+        for x in range(self.bounds.upper_left.x-2, self.bounds.lower_right.x+3):
+            for y in range(self.bounds.upper_left.y-2, self.bounds.lower_right.y+3):
+                pt = Point(x, y)
+                index = self.algorithm_index(pt)
+                new_image.bounds.grow(pt)
+                if algorithm[index] == '#':
+                    new_image.lit_pixels[pt] = True
+                else:
+                    new_image.lit_pixels[pt] = False
+
+        if algorithm[0] == '#':
+            new_image.infinite_pixels_on = not self.infinite_pixels_on
 
         return new_image
 
@@ -73,9 +125,6 @@ def solve(input_data_file: str):
         raw_input = dfile.read()
         algorithm, image_data = parse_input(raw_input)
         image = Image(image_data)
-        print(f"Number of pixels in original image: {len(image.lit_pixels)}")
         first_gen_image = image.apply_algorithm(algorithm)
-        print(f"Number of pixels in first generation: {len(first_gen_image.lit_pixels)}")
         second_gen_image = first_gen_image.apply_algorithm(algorithm)
-
-        print(f"Number of pixels on in second generation: {len(second_gen_image.lit_pixels)}")
+        print(f"Number of pixels on in second generation: {Counter(second_gen_image.lit_pixels.values())[True]}")
--- test/image_enhance_test.py
+++ test/image_enhance_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from image_enhance import parse_input, Image, Point
+from image_enhance import parse_input, Image, Point, Rect, ThawedPoint
 
 
 image_test_data = [
@@ -63,6 +63,11 @@ image_equality_test_data = [
      ['##.',
       '.##'],
      False),
+    (['.##',
+      '.##'],
+     ['##.',
+      '.##'],
+     False),
     (['.#.',
       '##.',
       '#.#'],
@@ -87,8 +92,16 @@ def test_image_equality_is_correct(input_image1, input_image2, expected_equal):
 
 
 example_algorithm_str = '..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#'
+puzzle_algorithm_str = '########..#..#.##.##.#.#..#.#.###.######.##..##.##..#...#....##.##...##.#.#...#.#.##.###.#.##.#.##.#...#.#.###.#.##.#.####.###..#.####.##..#.#####..####.#.#.#....##.#.#.##...#.####.#....#.##..##...#...#.##..#...#.#..#..#.#.#..##..#.##.##..##...#..###...##..#..###.###.##..#..#####...#.#..###..##....##...#####.#####...##.#.##.#....#.##.#.###.#.##.#.##...######.#...##.#..#.#.#...###.#..#.##.####..##.#..#.##.#.##.######.#.....#.#....####.####.###...#....#..###..###.#...#.#.#.##..#..##.#.#..#..###.#.###..#......'
 
 apply_algorithm_test_data = [
+    (puzzle_algorithm_str,
+     ['.#.'],
+     ['#######',
+      '#######',
+      '##.####',
+      '####.##',
+      '#######']),
     (example_algorithm_str,
      ['.#.'],
      ['.##',
@@ -154,4 +167,61 @@ def test_applying_enhancement_algorithm_produces_correct_image(input_algorithm, 
     image = Image(input_image_data)
     expected_image = Image(expected_image_data)
     enhanced_image = image.apply_algorithm(input_algorithm)
+    print(f"\nOriginal image: {image.bounds.upper_left.x}, {image.bounds.upper_left.y}")
+    print(f"                {image.bounds.lower_right.x}, {image.bounds.lower_right.y}")
+    image.dump()
+    print(f"Enhanced image: {enhanced_image.bounds.upper_left.x}, {enhanced_image.bounds.upper_left.y}")
+    print(f"                {enhanced_image.bounds.lower_right.x}, {enhanced_image.bounds.lower_right.y}")
+    enhanced_image.dump()
+    print(f"Expected image: {expected_image.bounds.upper_left.x}, {expected_image.bounds.upper_left.y}")
+    print(f"                {expected_image.bounds.lower_right.x}, {expected_image.bounds.lower_right.y}")
+    expected_image.dump()
     assert enhanced_image == expected_image
+
+
+apply_algorithm_twice_test_data = [
+    (puzzle_algorithm_str,
+     ['.#.'],
+     ['...........',
+      '....#......',
+      '....###....',
+      '......#....',
+      '...........']),
+    ]
+
+
+@pytest.mark.parametrize('input_algorithm, input_image_data, expected_image_data', apply_algorithm_twice_test_data)
+def test_applying_enhancement_twice_algorithm_produces_correct_image(input_algorithm, input_image_data, expected_image_data):
+    image = Image(input_image_data)
+    expected_image = Image(expected_image_data)
+    enhanced_image = image.apply_algorithm(input_algorithm)
+    second_enhanced_image = enhanced_image.apply_algorithm(input_algorithm)
+    print(f"\nOriginal image: {image.bounds.upper_left.x}, {image.bounds.upper_left.y}")
+    print(f"                {image.bounds.lower_right.x}, {image.bounds.lower_right.y}")
+    image.dump()
+    print(f"Enhanced image: {enhanced_image.bounds.upper_left.x}, {enhanced_image.bounds.upper_left.y}")
+    print(f"                {enhanced_image.bounds.lower_right.x}, {enhanced_image.bounds.lower_right.y}")
+    enhanced_image.dump()
+    print(f"2nd Enh. image: {second_enhanced_image.bounds.upper_left.x}, {second_enhanced_image.bounds.upper_left.y}")
+    print(f"                {second_enhanced_image.bounds.lower_right.x}, {second_enhanced_image.bounds.lower_right.y}")
+    second_enhanced_image.dump()
+    print(f"Expected image: {expected_image.bounds.upper_left.x}, {expected_image.bounds.upper_left.y}")
+    print(f"                {expected_image.bounds.lower_right.x}, {expected_image.bounds.lower_right.y}")
+    expected_image.dump()
+    assert second_enhanced_image == expected_image
+
+
+bounds_test_data = [
+    (['.##',
+      '#.#',
+      '.#.'],
+     Rect(ThawedPoint(0, 0), ThawedPoint(2, 2))
+     ),
+]
+
+
+@pytest.mark.parametrize('input_image_data, expected_bounds', bounds_test_data)
+def test_bounds_are_set_correctly_upon_image_creation(input_image_data, expected_bounds):
+    image = Image(input_image_data)
+
+    assert image.bounds == expected_bounds
